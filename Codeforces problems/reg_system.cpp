431841ff2bfa1e66e7e9c689cad963a0900e91b2
sarthak1905 <61883822+sarthak1905@users.noreply.github.com>
2020-10-28 18:03:23 +0530

    Added

---
+++ Codeforces problems/reg_system.cpp
@@ -0,0 +1,50 @@
+// Problem link= https://codeforces.com/problemset/problem/4/C
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long n;
+    long i,j,count;
+    cin >> n;
+    vector<string> database;
+    for(i = 0; i < n; ++i)
+    {
+        string temp;
+        cin >> temp;
+        database.push_back(temp);
+    }
+    for(i = 0; i < database.size(); ++i)
+    {
+        bool flag = true;
+        if(i==0)
+            cout << "OK";
+        else
+        {
+            string temp = database[i];
+            count = 0;
+            for(j = 0; j < i; ++j)
+            {
+                if(!temp.compare(database[j]))
+                {
+                    flag = false;
+                    count += 1;
+                    string num_app = to_string(count);
+                    if(count > 1)
+                        temp.erase(temp.size() - 1);
+                    temp.append(num_app);
+                }
+            }
+            if(flag)
+                cout << "\nOK";
+            else
+            {   
+                database[i] = temp;
+                cout << "\n" << temp;
+            }
+        }
+    }
+    return 0;
+}
