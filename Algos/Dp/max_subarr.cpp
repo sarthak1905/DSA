86887efd85c4ef5bba1cb75d2bd1a0ef94bf0424
Sarthak Saxena <sarthak.legendary@gmail.com>
2021-06-06 14:56:16 +0530

    Minor edit

--- Algos/Dp/max_subarr.cpp
+++ Algos/Dp/max_subarr.cpp
@@ -9,6 +9,7 @@ int main()
     vector<ll> arr;
     ll n, num, sum, best;
     cin >> n;
+    ll size = n;
     while(n--)
     {   
         cin >> num;
@@ -16,7 +17,7 @@ int main()
     }
     sum = arr[0];
     best = sum;
-    for(int i = 1; i < arr.size(); ++i)
+    for(int i = 1; i < size; ++i)
     {
         sum = max(sum + arr[i], arr[i]);
         best = max(sum, best);
